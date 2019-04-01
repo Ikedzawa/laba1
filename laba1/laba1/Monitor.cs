c99ed87e181b41814c17fc09e3a5aefa8b7b95d7
Ex <ikedzawa@mail.ru>
2019-04-01 10:11:14 +0300

    2-nd commit

---
+++ laba1/laba1/Monitor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1
+{
+    class Monitor
+    {
+        //firma string
+        //tip string
+        //diagonal float
+        public string firm { get; set; }
+        public string type { get; set; }
+        public float diagonal { get; set; }
+        public Monitor()
+        {
+            firm = "";
+            type = "";
+            diagonal = 0;
+        }
+        public void Input()
+        {
+            Console.WriteLine("Фирма");
+            firm = Console.ReadLine();
+            Console.WriteLine("Тип");
+            type = Console.ReadLine();
+            Console.WriteLine("Диагональ");
+            diagonal = float.Parse(Console.ReadLine());
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Фирма: {firm}");
+            Console.WriteLine($"Тип: {type}");
+            Console.WriteLine($"Диагональ: {diagonal}");
+        }
+    }
+}
